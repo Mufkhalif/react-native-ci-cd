6046b1ffdcc2e0899354f6be190a5e4810167e89
mufkhalif <mufkalif18@gmail.com>
2021-10-21 07:40:12 +0700

    update remove content

--- App.js
+++ App.js
@@ -61,15 +61,6 @@ const App = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">Selamat datang hayooo</Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
           <LearnMoreLinks />
         </View>
       </ScrollView>
